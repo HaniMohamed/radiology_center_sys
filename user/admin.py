f8fe70e68b833bc2a49aa1ee6f41c103b70937f4
Hani Hussein <hani.hussein94@gmail.com>
2021-06-07 00:02:24 +0200

    add department and bloodType in admin-site

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Department, Doctor, Patient
+from .models import Doctor, Patient
 
 # Register your models here.
-admin.site.register(Department)
 admin.site.register(Doctor)
 admin.site.register(Patient)
