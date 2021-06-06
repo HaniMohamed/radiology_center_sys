d23cfa2b6a9d08af8f80535c2176764a0df51dbe
Hani Hussein <hani.hussein94@gmail.com>
2021-06-06 23:10:51 +0200

    init database

--- elshawadfy_radiology/settings.py
+++ elshawadfy_radiology/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'elshawadfy_radiology.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elshwadfy_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
