1aee77b85dccff790c587c9552c29f7b862cae6e
Hani Hussein <hani.hussein94@gmail.com>
2021-06-06 23:42:05 +0200

    init departments-app

--- elshawadfy_radiology/settings.py
+++ elshawadfy_radiology/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #local apps
+    'user.apps.UserConfig',
+    'departments.apps.DepartmentsConfig',
 ]
 
 MIDDLEWARE = [
