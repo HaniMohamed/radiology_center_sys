a1a5ffd963b79fd8ea10cd2612430415828941f0
Hani Hussein <hani.hussein94@gmail.com>
2021-06-06 23:50:31 +0200

    extract department and bloodType models into external app

--- user/models.py
+++ user/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from departments.models import Department, BloodType
 
-# Create your models here.
+
+class Doctor(models.Model):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=50)
+    address = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Patient(models.Model):
+    name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=11)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=10)
+    blood_type = models.ForeignKey(BloodType, on_delete=models.CASCADE)
+    city = models.CharField(max_length=30)
+    supervisor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
