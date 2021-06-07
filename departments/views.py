9401a05bdba3d3055f7bfbca86b833e1a0b9d42d
Hani Hussein <hani.hussein94@gmail.com>
2021-06-08 00:46:36 +0200

    departments_index view with retrieving and show data from db

--- departments/views.py
+++ departments/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Department, BloodType
+from django.template import loader
 
-# Create your views here.
+
+def index(request):
+    departments = Department.objects.all()
+    context = {
+        'departments': departments,
+    }
+    return render(request, 'departments/index.html', context)
+
