67aacf34f9d6c8bed06819239e73f2e6cc5c1c89
Hani Hussein <hani.hussein94@gmail.com>
2021-06-19 01:58:44 +0200

    Heroku config

--- elshawadfy_radiology/settings.py
+++ elshawadfy_radiology/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import datetime
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -162,3 +163,4 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
