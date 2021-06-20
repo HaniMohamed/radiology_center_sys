244444ec57610f085646c1854707962c74880c48
Hani Hussein <hani.hussein94@gmail.com>
2021-06-20 22:20:06 +0200

    sqlite config

--- elshawadfy_radiology/settings.py
+++ elshawadfy_radiology/settings.py
@@ -14,6 +14,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -179,3 +181,5 @@ ALLOWED_HOSTS = ['*']
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
