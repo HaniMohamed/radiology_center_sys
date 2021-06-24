80ca786305a9f9a84e2bfc034a04afa3666c7d24
Hani Hussein <hani.hussein94@gmail.com>
2021-06-24 23:59:43 +0200

    fix some url_paths

--- user/urls.py
+++ user/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:pk>', views.UserDetails, name='user_details'),
-    path('doctors', views.DoctorList, name='doctors_list'),
-    path('patients', views.PatientList, name='patients_list'),
-    path('receptionists', views.ReceptionistList, name='receptionists_list'),
-    path('new', views.UserCreate, name='new_user'),
-    path('update/<int:pk>', views.UserCreate, name='update_user'),
-    path('delete/<int:pk>', views.UserDelete, name='delete_user'),
+    path('<int:pk>', views.UserDetails.as_view(), name='user_details'),
+    path('doctors', views.DoctorList.as_view(), name='doctors_list'),
+    path('patients', views.PatientList.as_view(), name='patients_list'),
+    path('receptionists', views.ReceptionistList.as_view(), name='receptionists_list'),
+    path('new', views.UserCreate.as_view(), name='new_user'),
+    path('update/<int:pk>', views.UserCreate.as_view(), name='update_user'),
+    path('delete/<int:pk>', views.UserDelete.as_view(), name='delete_user'),
 ]
