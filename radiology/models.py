e90ffe9aff650ebd2f41b37a05c56976917e7886
Hani Hussein <hani.hussein94@gmail.com>
2021-06-25 01:08:54 +0200

    make price editable

--- radiology/models.py
+++ radiology/models.py
@@ -23,7 +23,7 @@ class Examination(models.Model):
     supervisor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     notes = models.TextField(blank=True, null=True)
     radiology = models.ManyToManyField(Radiology)
-    total_price = models.DecimalField(max_digits=6, decimal_places=2, editable=False)
+    total_price = models.DecimalField(max_digits=6, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
