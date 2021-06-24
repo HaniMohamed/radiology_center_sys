d56a46ab9ee28690addb14e5a7c646c19fb8bc07
Hani Hussein <hani.hussein94@gmail.com>
2021-06-24 15:00:29 +0200

    fix update last login

--- auth/views.py
+++ auth/views.py
@@ -1,16 +1,16 @@
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework_simplejwt.views import TokenViewBase
 
 from auth.serializers import CustomTokenObtainPairSerializer
 
 
 class CustomTokenObtainPairView(TokenViewBase):
-
     serializer_class = CustomTokenObtainPairSerializer
+
 
 class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
