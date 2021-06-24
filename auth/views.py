3059b3d6ca85172077e8a3027cf250aca303655a
Hani Hussein <hani.hussein94@gmail.com>
2021-06-24 15:21:00 +0200

    fix logout view

--- auth/views.py
+++ auth/views.py
@@ -2,7 +2,6 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.token_blacklist.models import OutstandingToken, BlacklistedToken
 from rest_framework_simplejwt.views import TokenViewBase
 
 from auth.serializers import CustomTokenObtainPairSerializer
@@ -16,8 +15,6 @@ class LogoutView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-        tokens = OutstandingToken.objects.filter(user_id=request.user.id)
-        for token in tokens:
-            t, _ = BlacklistedToken.objects.get_or_create(token=token)
+        request.user.auth_token.delete()
 
         return Response(status=status.HTTP_205_RESET_CONTENT)
