50f7b5d942c614de1e1d72dc9aac1719bd1bf3fb
Hani Hussein <hani.hussein94@gmail.com>
2021-06-25 00:22:25 +0200

    fix userUpdate url

--- user/urls.py
+++ user/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('patients', views.PatientList.as_view(), name='patients_list'),
     path('receptionists', views.ReceptionistList.as_view(), name='receptionists_list'),
     path('new', views.UserCreate.as_view(), name='new_user'),
-    path('update/<int:pk>', views.UserCreate.as_view(), name='update_user'),
+    path('update/<int:pk>', views.UserUpdate.as_view(), name='update_user'),
     path('delete/<int:pk>', views.UserDelete.as_view(), name='delete_user'),
 ]
