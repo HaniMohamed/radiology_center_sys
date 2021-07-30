d8e141f8cdef57e23ebf20b5e727766d0a358d8a
Hani Hussein <hani.mohamed.hussein@gmail.com>
2021-07-30 06:40:44 +0200

    rename shift_id field to shift

--- radiology/models.py
+++ radiology/models.py
@@ -26,7 +26,7 @@ class Examination(models.Model):
     notes = models.TextField(blank=True, null=True)
     radiology = models.ManyToManyField(Radiology)
     total_price = models.DecimalField(max_digits=6, decimal_places=2)
-    shift_id = models.ForeignKey(Shift, on_delete=models.CASCADE, blank=True, null=True)
+    shift = models.ForeignKey(Shift, on_delete=models.CASCADE, blank=True, null=True)
     actual_price = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
