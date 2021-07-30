05113225eb7dd2a07353646c763b282b830899ba
Hani Hussein <hani.mohamed.hussein@gmail.com>
2021-07-30 06:38:33 +0200

    default shift's total_income

--- shifts/models.py
+++ shifts/models.py
@@ -12,3 +12,6 @@ class Shift(models.Model):
     closed = models.BooleanField(default=False, )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.receptionist + "(" + self.id + ")"
