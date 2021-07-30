44d5805a5774a88397906ed9b784b9ed30e38655
Hani Hussein <hani.mohamed.hussein@gmail.com>
2021-07-30 06:31:12 +0200

    default shift's total_income

--- shifts/models.py
+++ shifts/models.py
@@ -8,7 +8,7 @@ from user.models import CustomUser
 
 class Shift(models.Model):
     receptionist = ForeignKey(CustomUser, on_delete=models.CASCADE, )
-    total_income = models.DecimalField(max_digits=100, decimal_places=2)
+    total_income = models.DecimalField(max_digits=100, decimal_places=2, default=0.0)
     closed = models.BooleanField(default=False, )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
